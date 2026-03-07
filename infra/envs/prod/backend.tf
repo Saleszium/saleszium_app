3662ead870e4bd8ad1732edcc158b5a6cf974a72
prabhat-saleszium <prabhat@saleszium.com>
2026-03-07 09:47:13 +0530

    chore: Rename Terraform state S3 bucket from `saleszium-terraform-state` to `saleszium-tf-state`.

--- infra/envs/prod/backend.tf
+++ infra/envs/prod/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "saleszium-terraform-state"
+    bucket         = "saleszium-tf-state"
     key            = "envs/prod/terraform.tfstate"
     region         = "ap-south-1"
     dynamodb_table = "terraform-locks"
